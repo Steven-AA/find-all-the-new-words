034cb25ab894c6fdede491e2c05f6ab2dfa4132c
darksun <lujun9972@gmail.com>
2017-02-09 14:29:38 +0800

    done

--- find_all_i_donnot_know.py
+++ find_all_i_donnot_know.py
@@ -13,7 +13,7 @@ def real_word(word):
 
 def split_the_article(article):
     sep = re.compile('[\r\n., ]')
-    words = re.split(article)
+    words = re.split(sep,article)
     filcts = (real_word(word) for word in words)
     set_of_words = set(filcts)
     return set_of_words
@@ -22,7 +22,7 @@ def read_new_words(path,known_words):
     with open(path)as f:
         article = f.read()
     words = split_the_article(article)
-    print('there are {} words in {}'.format(num,path))
+    print('there are {} words in {}'.format(len(words),path))
     new_words = words - known_words
     num = len(new_words)
     if num == 0:
@@ -43,12 +43,13 @@ def get_name(path):
 
 def read_known_words():
     try:
-        with open ('../old.txt')as f:
+        with open ('./old.txt')as f:
             all_the_words = f.read()
     except:
         print('\'old.txt\' missing......')
         all_the_words = ""
     known_words = split_the_article(all_the_words)
+    print(known_words)
     num = len(known_words)
     print('There are ' + str(num)+' words I have known')
     return known_words
@@ -69,16 +70,15 @@ def learn(new_words,known_words):
         judge = my_input(word+'('+str(num)+' Left)\n')
         if judge == 1:
             known_words.add(word)
-            new_words.remove(word)
-    return new_words
+    return new_words - known_words
 
 def write_each_new_words(name,new_words):
     try:
-        os.makedirs('../new_words_of_each_article/')
+        os.makedirs('./new_words_of_each_article/')
     except:
         pass
     try:
-        with open('../new_words_of_each_article/'+name,'w') as f:
+        with open('./new_words_of_each_article/'+name,'w') as f:
             f.write('\n'.join(new_words))
     except:
         print('failed to creat file of \'./new_words_of_each_article/'+name+'\'')
@@ -89,12 +89,12 @@ if __name__ =='__main__':
         flag = input('print 1 to build model\nprint 2 to find model\nhere to get some help:https://zhuanlan.zhihu.com/p/25003457\n')
     new_word_all = set()
     known_words = read_known_words()
-    path = r'../articles'
+    path = r'./articles'
     file_names = get_name(path)
     print(file_names)
     for file_name in file_names:
-        print('opening' + each_filct + '......')
-        path_1 = path + '/' + each_filct
+        print('opening' + file_name + '......')
+        path_1 = path + '/' + file_name
         new_words = read_new_words(path_1,known_words)
         if new_words:
             if flag == 1:
@@ -103,11 +103,11 @@ if __name__ =='__main__':
                     print('new words:')
                     print(new_words)
                     write_each_new_words(file_name,new_words)
-                    new_word_all = new_word_all + set(new_words)
+                    new_word_all = new_word_all | set(new_words)
             else:
-                new_word_all = new_word_all + set(new_words)
-    with open('../old.txt','w') as old:
+                new_word_all = new_word_all | set(new_words)
+    with open('./old.txt','w') as old:
         old.write(' '.join(known_words))
-    with open('../new.txt','w') as new:
+    with open('./new.txt','w') as new:
         new.write('\n'.join(new_word_all))
     # print(ord(msvcrt.getch()))
